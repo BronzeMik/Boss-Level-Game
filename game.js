d7535fa525a562248d3c5d47be95456fb85999fb
Mikaila Brown <mbr2201@wgu.edu>
2023-12-07 21:46:17 -0500

    step one

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
 
+
+
+
+function nextSequence() {
+    randomNumber = Math.floor(Math.random() * 4);
+    
+    let randomChosencolor = randomNumber;
+    
+    gamePattern.push(randomChosenColor);
+}
